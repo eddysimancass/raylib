baadaea99a83d91f89671804a38941554df1c07e
Eddy <eduardosanchezsimancas@gmail.com>
2023-05-29 16:35:06 -0700

    test

--- raylib_template/src/main.c
+++ raylib_template/src/main.c
@@ -1,25 +1,68 @@
+/*******************************************************************************************
+*
+*   raylib [audio] example - Mixed audio processing
+*
+*   Example originally created with raylib 4.2, last time updated with raylib 4.2
+*
+*   Example contributed by hkc (@hatkidchan) and reviewed by Ramon Santamaria (@raysan5)
+*
+*   Example licensed under an unmodified zlib/libpng license, which is an OSI-certified,
+*   BSD-like license that allows static linking with closed source software
+*
+*   Copyright (c) 2023 hkc (@hatkidchan)
+*
+********************************************************************************************/
 #include "raylib.h"
-#include "second.h"
+#include <math.h>
 
-int main() 
+static float exponent = 1.0f;                 // Audio exponentiation value
+static float averageVolume[400] = { 0.0f };   // Average volume history
+
+//------------------------------------------------------------------------------------
+// Audio processing function
+//------------------------------------------------------------------------------------
+void ProcessAudio(void *buffer, unsigned int frames)
+{
+    float *samples = (float *)buffer;   // Samples internally stored as <float>s
+    float average = 0.0f;               // Temporary average volume
+
+    for (unsigned int frame = 0; frame < frames; frame++)
+    {
+        float *left = &samples[frame * 2 + 0], *right = &samples[frame * 2 + 1];
+
+        *left = powf(fabsf(*left), exponent) * ( (*left < 0.0f)? -1.0f : 1.0f );
+        *right = powf(fabsf(*right), exponent) * ( (*right < 0.0f)? -1.0f : 1.0f );
+
+        average += fabsf(*left) / frames;   // accumulating average volume
+        average += fabsf(*right) / frames;
+    }
+
+    // Moving history to the left
+    for (int i = 0; i < 399; i++) averageVolume[i] = averageVolume[i + 1];
+
+    averageVolume[399] = average;         // Adding last average value
+}
+
+//------------------------------------------------------------------------------------
+// Program main entry point
+//------------------------------------------------------------------------------------
+int main(void)
 {
     // Initialization
     //--------------------------------------------------------------------------------------
     const int screenWidth = 800;
     const int screenHeight = 450;
-	
-	Fun1();
 
-    InitWindow(screenWidth, screenHeight, "raylib");
+    InitWindow(screenWidth, screenHeight, "raylib [audio] example - processing mixed output");
 
-    Camera camera = { 0 };
-    camera.position = (Vector3){ 10.0f, 10.0f, 8.0f };
-    camera.target = (Vector3){ 0.0f, 0.0f, 0.0f };
-    camera.up = (Vector3){ 0.0f, 1.0f, 0.0f };
-    camera.fovy = 60.0f;
-    camera.projection = CAMERA_PERSPECTIVE;
-    
-    Vector3 cubePosition = { 0 };
+    InitAudioDevice();              // Initialize audio device
+
+    AttachAudioMixedProcessor(ProcessAudio);
+
+    Music music = LoadMusicStream("resources/country.mp3");
+    Sound sound = LoadSound("resources/coin.wav");
+
+    PlayMusicStream(music);
 
     SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
     //--------------------------------------------------------------------------------------
@@ -29,8 +72,17 @@ int main()
     {
         // Update
         //----------------------------------------------------------------------------------
-        UpdateCamera(&camera, CAMERA_ORBITAL);
+        UpdateMusicStream(music);   // Update music buffer with new stream data
+
+        // Modify processing variables
         //----------------------------------------------------------------------------------
+        if (IsKeyPressed(KEY_LEFT)) exponent -= 0.05f;
+        if (IsKeyPressed(KEY_RIGHT)) exponent += 0.05f;
+
+        if (exponent <= 0.5f) exponent = 0.5f;
+        if (exponent >= 3.0f) exponent = 3.0f;
+
+        if (IsKeyPressed(KEY_SPACE)) PlaySound(sound);
 
         // Draw
         //----------------------------------------------------------------------------------
@@ -38,17 +90,19 @@ int main()
 
             ClearBackground(RAYWHITE);
 
-            BeginMode3D(camera);
+            DrawText("MUSIC SHOULD BE PLAYING!", 255, 150, 20, LIGHTGRAY);
 
-                DrawCube(cubePosition, 2.0f, 2.0f, 2.0f, RED);
-                DrawCubeWires(cubePosition, 2.0f, 2.0f, 2.0f, MAROON);
-                DrawGrid(10, 1.0f);
+            DrawText(TextFormat("EXPONENT = %.2f", exponent), 215, 180, 20, LIGHTGRAY);
 
-            EndMode3D();
+            DrawRectangle(199, 199, 402, 34, LIGHTGRAY);
+            for (int i = 0; i < 400; i++)
+            {
+                DrawLine(201 + i, 232 - averageVolume[i] * 32, 201 + i, 232, MAROON);
+            }
+            DrawRectangleLines(199, 199, 402, 34, GRAY);
 
-            DrawText("This is a raylib example", 10, 40, 20, DARKGRAY);
-
-            DrawFPS(10, 10);
+            DrawText("PRESS SPACE TO PLAY OTHER SOUND", 200, 250, 20, LIGHTGRAY);
+            DrawText("USE LEFT AND RIGHT ARROWS TO ALTER DISTORTION", 140, 280, 20, LIGHTGRAY);
 
         EndDrawing();
         //----------------------------------------------------------------------------------
@@ -56,7 +110,13 @@ int main()
 
     // De-Initialization
     //--------------------------------------------------------------------------------------
-    CloseWindow();        // Close window and OpenGL context
+    UnloadMusicStream(music);   // Unload music stream buffers from RAM
+
+    DetachAudioMixedProcessor(ProcessAudio);  // Disconnect audio processor
+
+    CloseAudioDevice();         // Close audio device (music streaming is automatically stopped)
+
+    CloseWindow();              // Close window and OpenGL context
     //--------------------------------------------------------------------------------------
 
     return 0;
